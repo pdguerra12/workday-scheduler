3f8d8d3bfcf97597932fa3ff1c42049fda5bdcbd
Paul Guerra <paulguerra@Pauls-Air.home>
2021-12-02 15:06:20 -0600

    html and css edits, functions created to incorporate stlyes given

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 //starting global variables
-var currentTime = moment().hours();
+var presentTime = moment().hours();
 var now = moment().format("dddd, MMMM Do, YYYY");
 var timeBlock = $(".time-block");
 var saveBtn = $(".saveBtn");
 
 $("#currentDay").text(now);
+
+function ColorEl() {
+    timeBlock.each(function() {
+        var hour = $(this).attr("id");
+
+        if(presentTime > hour) {
+            $(this).addClass("past");
+        } else if (presentTime == hour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+}
+
+ColorEl();
