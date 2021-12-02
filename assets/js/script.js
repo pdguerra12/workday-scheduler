293209872159b6fea9902f5aaa9eee8d14e0b25f
Paul Guerra <paulguerra@Pauls-Air.home>
2021-12-01 22:34:15 -0600

    timeblock and savedbtn variables added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //starting global variables
 var currentTime = moment().hours();
-var now = moment().format('dddd, MMMM Do, YYYY');
+var now = moment().format("dddd, MMMM Do, YYYY");
+var timeBlock = $(".time-block");
+var saveBtn = $(".saveBtn");
 
 $("#currentDay").text(now);
